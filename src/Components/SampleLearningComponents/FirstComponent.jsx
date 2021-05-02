7c61e0993b3c36ff13eec4b8eb75f60fe222fdd4
rishav875 <goluchaurasia311990@gmail.com>
2021-05-02 12:14:40 +0530

    Learning Component in react

---
+++ src/Components/SampleLearningComponents/FirstComponent.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+//JSX
+//Creating Component using Class
+export default class FirstComponent extends Component{
+    render(){
+  return(
+    <div className="firstComponent">
+  Hello India!!!!
+  </div>
+  );
+  }
+  }
+ 
+  
---
+++ src/Components/SampleLearningComponents/SecondComponent.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+ //Creating Component using a function
+  function SecondComponent(){
+    return(
+      <div className="secondComponent">
+    Beaware of Corona
+    </div>
+    );  
+ }
+
+ export default SecondComponent;
